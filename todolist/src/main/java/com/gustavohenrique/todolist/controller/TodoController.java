1c3c23aafc2e382644fa31506c09fe07237a28d5
Gustavo <gustavosales0704@gmail.com>
2024-11-27 23:32:57 -0300

    Alterações na camada Controller

--- todolist/src/main/java/com/gustavohenrique/todolist/controller/TodoController.java
+++ todolist/src/main/java/com/gustavohenrique/todolist/controller/TodoController.java
@@ -1,4 +1,47 @@
 package com.gustavohenrique.todolist.controller;
 
+import com.gustavohenrique.todolist.entity.Todo;
+import com.gustavohenrique.todolist.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
 public class TodoController {
+    TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping("/todos")
+    List<Todo> create(@RequestBody Todo todo) {
+        return todoService.create(todo);
+    }
+
+    @GetMapping("/todos")
+    List<Todo> list() {
+        return todoService.list();
+    }
+
+    @GetMapping("/todos/{id}")
+    List<Todo> list(@PathVariable(value = "id") UUID id) {
+        return todoService.list();
+    }
+
+    /*@GetMapping("/todos/{realizado}")
+    List<Todo> list(@PathVariable("true") boolean realizado) {
+        return todoService.list();
+    }*/
+
+    /*@PutMapping("/todos/{id}")
+    List<Todo> update(@RequestBody Todo todo) {
+        return todoService.update(todo);
+    }*/
+
+    @DeleteMapping("/todos/{id}")
+    List<Todo> delete(@PathVariable("id") UUID id) {
+        return todoService.delete(id);
+    }
 }
